012ce2d11c1f5db602856561d70dd8dfc77e74d3
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-11-29 18:33:33 -0800

    On video 16

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,7 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("CesarLevel01");
                 
-               this.resetPlayer();
+               this.resetPlayer(0, 400);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
 		// add our HUD to the game world
@@ -25,9 +25,9 @@ game.PlayScreen = me.ScreenObject.extend({
 		me.game.world.removeChild(this.HUD);
 	},
         
-        resetPlayer: function(){
-             var player = me.pool.pull("mario", 0, 420, {});
-             me.game.world.addChild(player, 3);
+        resetPlayer: function(x, y){
+             var player = me.pool.pull("mario", x, y, {});
+             me.game.world.addChild(player, 5);
         }
         
         
