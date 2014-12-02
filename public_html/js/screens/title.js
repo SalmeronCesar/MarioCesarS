a84b54342e521831754090402e37256ef404fc79
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-12-02 14:03:10 -0800

    Just working on the map a lil

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -15,7 +15,7 @@ game.TitleScreen = me.ScreenObject.extend({
                     },
                     
                     draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+                        this.font.draw(renderer.getContext(), "Welcome Mate!", 450, 130);
                         this.font.draw(renderer.getContext(), "Press Enter to play",250 , 530);
                     }
                     
