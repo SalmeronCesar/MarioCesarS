77c2ebee433de55d45ef41de754e0793dc6ebf1e
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-12-05 14:04:55 -0800

    Got to go left and jump not fly

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,27 +13,39 @@ game.PlayerEntity = me.Entity.extend({
         }]);
     
     this.renderable.addAnimation("idle", [3]);
+    //Creates an animation called smallWalk using pictures of the image defined above (mario)
+    //Sets the animation to run through pictures 8-13
+    // The last number says we switch between pictures every 80 milliseconds
     this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
     
     this.renderable.setCurrentAnimation("idle");
-        
+        //Sets the speed we go on the X-axis(firstnumber) and Y-axis(second number)
         this.body.setVelocity(5, 20);
+        //Sets the camera (viewport) to follow mario's position (pos) on both the X and Y axis
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     
     update: function(delta){
-        if(me.input.isKeyPressed("right")){
+        //Checks if the right key is pressed and if it is, execute the following statement.
+         if(me.input.isKeyPressed("right")){
+            this.flipX(false)
+            //Sets the position of mario onn the X-axis by adding the x value from the setVelocity times the timer.tick
+            //me.timer.tick uses the time since last animation to make the distance traveled smooth
             this.body.vel.x +=this.body.accel.x * me.timer.tick;
             
         }
         else if(me.input.isKeyPressed("left")){
+            this.flipX(true);
             this.body.vel.x -=this.body.accel.x * me.timer.tick;
         }
         else{
             this.body.vel.x = 0;
         }
-          if(me.input.isKeyPressed("space")){
-            this.body.vel.y -=this.body.accel.y * me.timer.tick;
+          if(me.input.isKeyPressed("jump")){
+              if(!this.body.jumping && !this.body.falling) {
+              this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+            this.body.jumping = true;
+        }
         }
         
         this.body.update(delta);
@@ -62,6 +74,7 @@ game.PlayerEntity = me.Entity.extend({
 game.LevelTrigger = me.Entity.extend({
    init: function(x, y, settings){
        this._super(me.Entity, 'init', [x, y, settings]);
+       //If something collides with this object when it will triiger that image or thing to another place
        this.body.onCollision = this.onCollision.bind(this);
        this.level = settings.level;
        this.xSpawn = settings.xSpawn;
@@ -69,6 +82,8 @@ game.LevelTrigger = me.Entity.extend({
    },
    
    onCollision: function(){
+       //Sets this object so that it will collide only with objects or type no_object, which don't exist 
+       //so really, makes it so this object will not collide with anything anymore
        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
        me.levelDirector.loadLevel(this.level);
        me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -15,7 +15,7 @@ game.TitleScreen = me.ScreenObject.extend({
                     },
                     
                     draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Welcome Mate", 450, 130);
+                        this.font.draw(renderer.getContext(), "Mario Maybe?", 400, 130);
                         this.font.draw(renderer.getContext(), "Press Enter to play",250 , 530);
                     }
                     
