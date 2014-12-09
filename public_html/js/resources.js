940748bab6abd7310b4bb91fb570afd8b3dd98e0
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-12-09 06:47:14 -0800

    Added a better looking title screen

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -7,7 +7,7 @@ game.resources = [
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "mario", type:"image", src: "data/img/player1.png"},
-        {name: "title-screen", type:"image", src: "data/img/super mario 1.png"},
+        {name: "title-screen", type:"image", src: "data/img/mario.jpg"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
